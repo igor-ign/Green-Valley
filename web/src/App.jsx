cc85f4e1106be54689ce0e7855804419b885a07c
Igor Ignácio <igor.ignacio@cwi.com.br>
2023-05-08 09:56:26 -0300

    feat: admin login screen

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { ROUTES } from "./app-constants/routes";
+import { Login } from "./pages/login";
 import "./index.css";
 
 function AppRoutes() {
   return (
     <div className="app-routes">
       <Routes>
-        <Route path={ROUTES.LOGIN} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
       </Routes>
     </div>
   );
--- web/src/pages/login/index.jsx
+++ web/src/pages/login/index.jsx
@@ -1,5 +1,18 @@
 import "./style.scss";
 
 export function Login() {
-  return <div>Login</div>;
+  return (
+    <form className="login-form">
+      <div className="input-container">
+        <label htmlFor="login">Login</label>
+        <input type="text" id="login" placeholder="admin@gmail.com" />
+      </div>
+      <div className="input-container">
+        <label htmlFor="login">Password</label>
+        <input type="text" id="password" placeholder="Your password" />
+      </div>
+
+      <button className="submit-button">Log in</button>
+    </form>
+  );
 }
