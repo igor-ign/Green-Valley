897a3f31ded36a09167d74add2ca4415631f7f01
Igor Ignácio <igor.ignacio@cwi.com.br>
2023-05-11 14:04:11 -0300

    feat: item navigation

--- web/src/components/admin-header/components/index.jsx
+++ web/src/components/admin-header/components/index.jsx
@@ -1,15 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { ExitToApp } from "@material-ui/icons";
-import { ADMIN_NAVBAR_ITEMS } from "../../../app-constants";
+import { ADMIN_NAVBAR_ITEMS, ROUTES } from "../../../app-constants";
 import "./style.scss";
 
 export function AdminNavbar({ isOpen }) {
+  const navigate = useNavigate();
   return (
     <nav className={`${isOpen ? "nav--open" : "nav--closed"}`}>
       <ul className="nav__items">
         {ADMIN_NAVBAR_ITEMS.map((item) => {
           return (
             <li key={item.id} className="nav__item">
-              <button className="item__button">
+              <button
+                className="item__button"
+                onClick={() => navigate(item.path)}
+              >
                 <div className="item__icon">{item.icon}</div>
                 <h3 className="item__name">{item.name}</h3>
               </button>
@@ -17,7 +22,7 @@ export function AdminNavbar({ isOpen }) {
           );
         })}
       </ul>
-      <button className="nav__logout">
+      <button className="nav__logout" onClick={() => navigate(ROUTES.LOGIN)}>
         <div className="logout__icon">
           <ExitToApp />
         </div>
