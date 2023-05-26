b6956dc8b7a53adca59fd6271df2f10f3b0eb722
Igor Ignácio <igor.ignacio@cwi.com.br>
2023-05-26 10:27:56 -0300

    feat: add list component styles to the page component

--- web/src/components/admin-list/components/list-header/index.jsx
+++ web/src/components/admin-list/components/list-header/index.jsx
@@ -1,12 +1,11 @@
 export function ListHeader({ headerItems, possibleActions }) {
-  // TODO: Think about letting the components who have this one as a child to decide how SOME styles will behave
   return (
     <header className="list__header">
       <ul className="header__list" type="none">
         {headerItems.map((item) => {
           return (
             <li
-              className={`list__item ${
+              className={`header__list__item ${
                 headerItems.indexOf(item) === 0 ? "--id" : ""
               }`}
             >
