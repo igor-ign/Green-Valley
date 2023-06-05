133214a6567a746aed930f76daa227e9871b3d76
Igor Ignácio <igor.ignacio@cwi.com.br>
2023-06-05 09:39:10 -0300

    feat: house request

--- api/src/main/java/br/com/gv/api/controller/HouseController.java
+++ api/src/main/java/br/com/gv/api/controller/HouseController.java
@@ -1,17 +1,20 @@
 package br.com.gv.api.controller;
 
+import br.com.gv.api.controller.request.HouseRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/houses")
 public class HouseController {
 
     @PostMapping("/add")
-    public HttpStatus addNewHouse(@RequestBody HouseRequest request) {
+    public HttpStatus addNewHouse(@Valid @RequestBody HouseRequest request) {
         return houseService.addNewHouse();
     }
 }
---
+++ api/src/main/java/br/com/gv/api/controller/request/HouseRequest.java
@@ -0,0 +1,44 @@
+package br.com.gv.api.controller.request;
+
+import br.com.gv.api.enumeration.BuildingType;
+import br.com.gv.api.enumeration.NegociationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HouseRequest {
+
+    @NotNull(message = "Please, inform the neighborhood of this house.")
+    private UUID neighborhoodId;
+
+    private String street;
+
+    @NotNull(message = "Please, inform the negociation type of this house (SELL / RENT).")
+    private NegociationType negociationType;
+
+    @NotNull(message = "Please, inform the building type of this house (HOUSE / APARTMENT).")
+    private BuildingType buildingType;
+
+    @NotNull(message = "Please, inform the price of this house.")
+    private int price;
+
+    @NotNull(message = "Please, inform the number of bedrooms of this house.")
+    private int bedrooms;
+
+    @NotNull(message = "Please, inform the number of bathrooms of this house.")
+    private int bathrooms;
+
+    @NotNull(message = "Please, inform the size of this house.")
+    private int houseSize;
+
+    private String description;
+}
