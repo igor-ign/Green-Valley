c88c112a96d55138fe64bf98c824c16e928cb6a5
Igor Ignácio <igor.ignacio@cwi.com.br>
2023-06-14 17:34:26 -0300

    refact: house edit dto

--- api/src/main/java/br/com/gv/api/controller/request/HouseEditRequest.java
+++ api/src/main/java/br/com/gv/api/controller/request/HouseEditRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
@@ -13,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class HouseEditRequest {
 
+    @NotNull(message = "House Id cannot be empty.")
     private UUID houseId;
 
     private UUID neighborhoodId;
