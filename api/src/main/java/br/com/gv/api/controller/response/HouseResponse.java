07a28438e8cb727f33e942365e435154951177de
Igor Ignácio <igor.ignacio@cwi.com.br>
2023-06-29 18:47:28 -0300

    refact: add house id to response dto

--- api/src/main/java/br/com/gv/api/controller/response/HouseResponse.java
+++ api/src/main/java/br/com/gv/api/controller/response/HouseResponse.java
@@ -2,12 +2,16 @@ package br.com.gv.api.controller.response;
 
 import lombok.*;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class HouseResponse {
+
+    private UUID id;
 
     private String location;
 
